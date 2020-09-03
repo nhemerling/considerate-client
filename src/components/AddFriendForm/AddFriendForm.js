a29c1753c3a602c9f9cd5042bda54265c1cf9164
nhemerling <65194792+nhemerling@users.noreply.github.com>
2020-09-03 12:08:34 -0700

    add form for adding likes (#2)

--- src/components/AddFriendForm/AddFriendForm.js
+++ src/components/AddFriendForm/AddFriendForm.js
@@ -1,71 +1,36 @@
 import React, { Component } from 'react';
-import FriendApiService from '../../services/friend-api-service';
+import FriendStage from './FriendStage';
+import LikesStage from './LikesStage';
 
 export default class AddFriendForm extends Component {
-  static defaultProps = {
-    onAddFriendSuccess: () => {},
+  constructor(props) {
+    super(props);
+    this.state = {
+      friend: {},
+    };
+  }
+
+  handleFriendStageSuccess = (friend) => {
+    this.setState({
+      friend,
+    });
   };
 
-  state = { error: null };
-
-  handleSubmit = (ev) => {
-    ev.preventDefault();
-    const { friend_name, occasion, occasion_date } = ev.target;
-    const newFriend = {
-      friend_name: friend_name.value,
-      occasion: occasion.value,
-      occasion_date: occasion_date.value,
-    };
-
-    this.setState({ error: null });
-
-    FriendApiService.postFriend(newFriend)
-      .then((friend) => {
-        friend_name.value = '';
-        occasion.value = '';
-        occasion_date.value = '';
-        this.props.onAddFriendSuccess();
-      })
-      .catch((res) => {
-        this.setState({ error: res.error });
-      });
+  handleLikesStageSuccess = (likes) => {
+    this.setState({});
   };
 
   render() {
-    const { error } = this.state;
+    const { friend } = this.state;
+    console.log(friend);
     return (
-      <form className="AddFriendForm" onSubmit={this.handleSubmit}>
-        <div role="alert">{error && <p className="red">{error}</p>}</div>
-        <div className="friend_name">
-          <label htmlFor="AddFriendForm__friend_name">Friend name</label>
-          <input
-            name="friend_name"
-            type="text"
-            required
-            id="AddFriendForm__friend_name"
-          ></input>
-        </div>
-        <div className="occasion">
-          <label htmlFor="AddFriendForm__occasion">Occasion</label>
-          <input
-            name="occasion"
-            type="text"
-            required
-            id="AddFriendForm__occasion"
-          ></input>
-        </div>
-        <div className="occasion_date">
-          <label htmlFor="AddFriendForm__occasion_date">Occasion Date</label>
-          <input
-            name="occasion_date"
-            type="date"
-            required
-            id="AddFriendForm__occasion_date"
-          ></input>
-        </div>
-        <button type="reset">Reset</button>
-        <button type="submit">Add</button>
-      </form>
+      <section className="AddFriendForm">
+        <FriendStage onFriendStageSuccess={this.handleFriendStageSuccess} />
+        <LikesStage
+          friend={friend}
+          onLikesStageSuccess={this.handleLikesStageSuccess}
+        />
+      </section>
     );
   }
 }
