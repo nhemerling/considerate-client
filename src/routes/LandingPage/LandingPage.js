2ed9af17605528e8f503dc195c3f4670156da8d8
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-10 11:51:07 -0700

    small edit to copy on landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -13,7 +13,7 @@ export default class LandingPage extends Component {
             <h3>Give the perfect gift, every time.</h3>
           </header>
           <p>
-            Considerate helps you keep track of the things your friends love and
+            Considerate helps you keep track of the things your friends like and
             of the occasions when you'd like to give them the perfect gift.
           </p>
           <p>You can be the thoughtful friend you've always meant to be.</p>
@@ -21,15 +21,15 @@ export default class LandingPage extends Component {
 
         <section className="LandingPage__section">
           <header>
-            <h3>Keep track of what your friends love.</h3>
+            <h3>Keep track of what your friends like.</h3>
           </header>
           <p>
             The key to giving the perfect gift is remembering what your friends
-            love.
+            like.
           </p>
           <p>
             Considerate provides you with an intuitive interface that helps you
-            keep track of all the things your friends have told you they love.
+            keep track of all the things your friends have told you they like.
           </p>
         </section>
 
@@ -38,7 +38,7 @@ export default class LandingPage extends Component {
             <h3>We'll remind you.</h3>
           </header>
           <p>
-            When you sign in, the first thing you'll see is your friends with
+            When you sign in, the home page will show you your friends with
             upcoming special occassions.
           </p>
           <p>That way you'll never miss another anniversay.</p>
