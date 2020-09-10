acba62675691b81050b20b3eb6522bcb400d31d0
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-10 12:02:54 -0700

    change api endpoint in config to prod server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
   TOKEN_KEY: process.env.TOKEN_KEY || 'considerate-client-auth-token',
-  API_ENDPOINT:
-    process.env.REACT_APP_API_ENDPOINT ||
-    'https://immense-bayou-75450.herokuapp.com/api',
+  API_ENDPOINT: 'https://immense-bayou-75450.herokuapp.com/api',
 };
