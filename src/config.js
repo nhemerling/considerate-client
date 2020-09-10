bb07a427e1f6d4f2bfc4b62d2074323c9a998862
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-10 13:06:17 -0700

    small config change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
   TOKEN_KEY: process.env.TOKEN_KEY || 'considerate-client-auth-token',
-  API_ENDPOINT: 'https://immense-bayou-75450.herokuapp.com/api',
+  API_ENDPOINT:
+    process.env.REACT_APP_API_ENDPOINT ||
+    'https://immense-bayou-75450.herokuapp.com/api',
 };
